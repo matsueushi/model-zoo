10213005a1c1736c6579aa36271fe19d38c25717
matsueushi <matsueushi@gmail.com>
2020-02-29 17:21:25 +0000

    Cleanup

--- vision/mnist/dcgan.jl
+++ vision/mnist/dcgan.jl
@@ -10,11 +10,9 @@ using Printf
 const BATCH_SIZE = 128
 const NOISE_DIM = 100
 const EPOCHS = 15
-const VERBOSE_FREQ = 500
+const VERBOSE_FREQ = 1000
 const ANIMATION_X = 6
 const ANIMATION_Y = 6
-
-result_dir = "mnist-dcgan-results"
 
 # Taking vector of images and return minibatch
 function make_minibatch(xs)
@@ -22,31 +20,23 @@ function make_minibatch(xs)
     return @. 2f0 * ys - 1f0
 end
 
-function generator_loss(fake_output)
-    loss = mean(logitbinarycrossentropy.(fake_output, 1f0))
-    return loss 
-end
-
-function discriminator_loss(real_output, fake_output)
-    real_loss = mean(logitbinarycrossentropy.(real_output, 1f0))
-    fake_loss = mean(logitbinarycrossentropy.(fake_output, 0f0))
-    loss = 0.5f0 * (real_loss + fake_loss)
-    return loss
-end
-
-function convert_to_image(image_array)
-    image_array = @. (image_array + 1f0) / 2f0
-    return Gray.(image_array)
-end
-
 function save_fake_image(gen, animation_noise, train_steps)
     @eval Flux.istraining() = false
     fake_images = @. cpu(gen(animation_noise))
     @eval Flux.istraining() = true
     image_array = dropdims(reduce(vcat, reduce.(hcat, partition(fake_images, ANIMATION_Y))); dims=(3, 4))
-    output_image = convert_to_image(image_array)
-    save(@sprintf("%s/steps_%06d.png", result_dir, train_steps), output_image)
+    image_array = @. Gray(image_array + 1f0) / 2f0
+    return convert_to_image(image_array)
 end
+
+# Loss functions
+function discriminator_loss(real_output, fake_output)
+    real_loss = mean(logitbinarycrossentropy.(real_output, 1f0))
+    fake_loss = mean(logitbinarycrossentropy.(fake_output, 0f0))
+    return real_loss + fake_loss
+end
+
+generator_loss(fake_output) = mean(logitbinarycrossentropy.(fake_output, 1f0))
 
 function train_discriminator!(gen, dscr, batch, opt_dscr)
     noise = randn(Float32, NOISE_DIM, BATCH_SIZE) |> gpu
@@ -74,15 +64,22 @@ function train_generator!(gen, dscr, batch, opt_gen)
 end
 
 function train()
-    if !isdir(result_dir)
-        mkdir(result_dir)
-    end
-
     # MNIST dataset
     images = MNIST.images()
     data = [make_minibatch(xs) |> gpu for xs in partition(images, BATCH_SIZE)]
 
     animation_noise = [randn(NOISE_DIM, 1) |> gpu for _=1:ANIMATION_X*ANIMATION_Y]
+
+    # Discriminator
+    dscr =  Chain(
+        Conv((4, 4), 1 => 64; stride = 2, pad = 1),
+        x->leakyrelu.(x, 0.2f0),
+        Dropout(0.25),
+        Conv((4, 4), 64 => 128; stride = 2, pad = 1),
+        x->leakyrelu.(x, 0.2f0),
+        Dropout(0.25), 
+        x->reshape(x, 7 * 7 * 128, :),
+        Dense(7 * 7 * 128, 1)) |> gpu
 
     # Generator
     gen = Chain(
@@ -96,32 +93,24 @@ function train()
         ConvTranspose((4, 4), 64 => 1, tanh; stride = 2, pad = 1),
         ) |> gpu
 
-    # Discriminator
-    dscr =  Chain(
-        Conv((4, 4), 1 => 64; stride = 2, pad = 1),
-        x->leakyrelu.(x, 0.2f0),
-        Dropout(0.25),
-        Conv((4, 4), 64 => 128; stride = 2, pad = 1),
-        x->leakyrelu.(x, 0.2f0),
-        Dropout(0.25), 
-        x->reshape(x, 7 * 7 * 128, :),
-        Dense(7 * 7 * 128, 1)) |> gpu
-
-    opt_gen = ADAM(0.0002)
+    # Optimizers
     opt_dscr = ADAM(0.0002)
+    opt_gen = ADAM(0.0002)
 
     # Training
     train_steps = 0
     for ep in 1:EPOCHS
         @info "Epoch $ep"
         for batch in data
+            # Update discriminator and generator
             disc_loss = train_discriminator!(gen, dscr, batch, opt_dscr)
             gen_loss = train_generator!(gen, dscr, batch, opt_gen)
 
             if train_steps % VERBOSE_FREQ == 0
                 @info("Train step $(train_steps), Discriminator loss: $(disc_loss), Generator loss: $(gen_loss)")
                 # create fake images for animation
-                save_fake_image(gen, animation_noise, train_steps)
+                output_image = save_fake_image(gen, animation_noise, train_steps)
+                save(@sprintf("dcgan_steps_%06d.png", train_steps), output_image)
             end
             train_steps += 1
         end
@@ -129,4 +118,5 @@ function train()
 
 end
 
+cd(@__DIR__)
 train()
