ef7c301eb72993e8cc82f2fe8dc7be887bfcabd3
matsueushi <matsueushi@gmail.com>
2020-02-29 15:09:20 +0000

    Refactoring...

--- vision/mnist/dcgan2.jl
+++ vision/mnist/dcgan2.jl
@@ -1,5 +1,4 @@
 using Base.Iterators: partition
-using DelimitedFiles
 using Flux
 using Flux.Data.MNIST
 using Flux.Optimise: update!
@@ -8,14 +7,13 @@ using Images
 using Statistics
 using Printf
 
-animation_dir = "mnist-dcgan-animation"
+const BATCH_SIZE = 128
+const NOISE_DIM = 100
+const EPOCHS = 15
+
 result_dir = "mnist-dcgan-results"
 
 mutable struct DCGAN
-    noise_dim::Int64
-    channels::Int64
-    batch_size::Int64
-
     generator::Chain
     discriminator::Chain
 
@@ -33,18 +31,16 @@ mutable struct DCGAN
     discriminator_loss_hist::Vector{Float32}
 end
 
-function DCGAN(; image_vector::Vector{<: AbstractMatrix}, noise_dim::Int64, channels::Int64, batch_size::Int64, 
+function DCGAN(; image_vector::Vector{<: AbstractMatrix},
     generator::Chain, discriminator::Chain,
     animation_size::Pair{Int64, Int64}, verbose_freq::Int64)
 
-    @assert (channels == 1) || (channels == 3)
-
-    data = [reshape(reduce(hcat, channelview.(xs)), 28, 28, 1, :) for xs in partition(image_vector, batch_size)]
+    data = [reshape(reduce(hcat, channelview.(xs)), 28, 28, 1, :) for xs in partition(image_vector, BATCH_SIZE)]
     data = [2f0 .* gpu(Float32.(xs)) .- 1f0 for xs in data]
 
-    animation_noise = randn(Float32, noise_dim, prod(animation_size)) |> gpu
+    animation_noise = randn(Float32, NOISE_DIM, prod(animation_size)) |> gpu
 
-    DCGAN(noise_dim, channels, batch_size, generator, discriminator, ADAM(0.0001f0), ADAM(0.0001f0), data, 
+    DCGAN(generator, discriminator, ADAM(0.0001f0), ADAM(0.0001f0), data, 
         animation_size, animation_noise, 0, verbose_freq, Vector{Float32}(), Vector{Float32}())
 end
 
@@ -60,13 +56,9 @@ function discriminator_loss(real_output, fake_output)
     return loss
 end
 
-function convert_to_image(image_array::Matrix{Float32}, channels::Int64)
+function convert_to_image(image_array::Matrix{Float32})
     image_array = @. (image_array + 1f0) / 2f0
-    if channels == 1
-        return Gray.(image_array)
-    else
-        return colorview(RGB, image_array)
-    end
+    return Gray.(image_array)
 end
 
 function save_fake_image(dcgan::DCGAN)
@@ -81,12 +73,12 @@ function save_fake_image(dcgan::DCGAN)
         i = n % cols
         tile_image[j * h + 1:(j + 1) * h, i * w + 1:(i + 1) * w] = fake_images[:, :, :, n + 1] |> cpu
     end
-    image = convert_to_image(tile_image, dcgan.channels)
+    image = convert_to_image(tile_image)
     save(@sprintf("%s/steps_%06d.png", result_dir, dcgan.train_steps), image)
 end
 
 function train_discriminator!(dcgan::DCGAN, batch::AbstractArray{Float32, 4})
-    noise = randn(Float32, dcgan.noise_dim, dcgan.batch_size) |> gpu
+    noise = randn(Float32, NOISE_DIM, BATCH_SIZE) |> gpu
     fake_input = dcgan.generator(noise)
     loss(m) = discriminator_loss(m(batch), m(fake_input))
     disc_grad = gradient(()->loss(dcgan.discriminator), Flux.params(dcgan.discriminator))
@@ -95,7 +87,7 @@ function train_discriminator!(dcgan::DCGAN, batch::AbstractArray{Float32, 4})
 end
 
 function train_generator!(dcgan::DCGAN, batch::AbstractArray{Float32, 4})
-    noise = randn(Float32, dcgan.noise_dim, dcgan.batch_size) |> gpu
+    noise = randn(Float32, NOISE_DIM, BATCH_SIZE) |> gpu
     loss(m) = generator_loss(dcgan.discriminator(m(noise)))
     gen_grad = gradient(()->loss(dcgan.generator), Flux.params(dcgan.generator))
     update!(dcgan.generator_optimizer, Flux.params(dcgan.generator), gen_grad)
@@ -104,14 +96,12 @@ end
 
 function train!(dcgan::DCGAN, epochs::Integer)
     for ep in 1:epochs
-        @info "epoch $ep"
+        @info "Epoch $ep"
         for batch in dcgan.data
             disc_loss = train_discriminator!(dcgan, batch)
             gen_loss = train_generator!(dcgan, batch)
 
             if dcgan.train_steps % dcgan.verbose_freq == 0
-                push!(dcgan.discriminator_loss_hist, disc_loss)
-                push!(dcgan.generator_loss_hist, gen_loss)
                 @info("Train step $(dcgan.train_steps), Discriminator loss: $(disc_loss), Generator loss: $(gen_loss)")
                 # create fake images for animation
                 save_fake_image(dcgan)
@@ -123,51 +113,33 @@ end
 
 
 function main()
-    if !isdir(animation_dir)
-        mkdir(animation_dir)
-    end
-
     if !isdir(result_dir)
         mkdir(result_dir)
     end
 
-    noise_dim = 100
-    channels = 1
-
     generator = Chain(
-        Dense(noise_dim, 7 * 7 * 256; initW = glorot_normal),
+        Dense(NOISE_DIM, 7 * 7 * 256),
         BatchNorm(7 * 7 * 256, relu),
         x->reshape(x, 7, 7, 256, :),
-        ConvTranspose((5, 5), 256 => 128; init = glorot_normal, stride = 1, pad = 2),
+        ConvTranspose((5, 5), 256 => 128; stride = 1, pad = 2),
         BatchNorm(128, relu),
-        ConvTranspose((4, 4), 128 => 64; init = glorot_normal, stride = 2, pad = 1),
+        ConvTranspose((4, 4), 128 => 64; stride = 2, pad = 1),
         BatchNorm(64, relu),
-        ConvTranspose((4, 4), 64 => channels, tanh; init = glorot_normal, stride = 2, pad = 1),
+        ConvTranspose((4, 4), 64 => 1, tanh; stride = 2, pad = 1),
         ) |> gpu
 
     discriminator =  Chain(
-        Conv((4, 4), channels => 64, leakyrelu; init = glorot_normal, stride = 2, pad = 1),
+        Conv((4, 4), 1 => 64, leakyrelu; stride = 2, pad = 1),
         Dropout(0.25),
-        Conv((4, 4), 64 => 128, leakyrelu; init = glorot_normal, stride = 2, pad = 1),
+        Conv((4, 4), 64 => 128, leakyrelu; stride = 2, pad = 1),
         Dropout(0.25), 
         x->reshape(x, 7 * 7 * 128, :),
-        # drop sigmoid, and use logitbinarycrossentropy (it is more numerically stable)
-        # https://github.com/FluxML/Flux.jl/issues/914
-        Dense(7 * 7 * 128, 1; initW = glorot_normal)) |> gpu 
+        Dense(7 * 7 * 128, 1)) |> gpu 
 
-    dcgan = DCGAN(; image_vector = MNIST.images(), noise_dim = noise_dim, 
-        channels = channels, batch_size = 128,
+    dcgan = DCGAN(; image_vector = MNIST.images(),
         generator = generator, discriminator = discriminator,
         animation_size = 6=>6, verbose_freq = 100)
     train!(dcgan, 30)
-
-    open(@sprintf("%s/discriminator_loss.txt", result_dir), "w") do io
-        writedlm(io, dcgan.discriminator_loss_hist)
-    end
-
-    open(@sprintf("%s/generator_loss.txt", result_dir), "w") do io
-        writedlm(io, dcgan.generator_loss_hist)
-    end
 
 end
 
